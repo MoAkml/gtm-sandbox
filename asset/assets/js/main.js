061db3a42cd655da8d49b28862c79dbfd4847b3b
MoAkml <mohamadakml63@gmail.com>
2026-01-17 02:08:27 +0200

    Track link clicks with dataLayer push
    Add event listener to track link clicks with dataLayer.

--- asset/assets/js/main.js
+++ asset/assets/js/main.js
@@ -1,1 +1,10 @@
 
+document.querySelectorAll("a").forEach(link => {
+  link.addEventListener("click", () => {
+    window.dataLayer.push({
+      event: "link_click",
+      link_text: link.innerText,
+      link_url: link.href
+    });
+  });
+});
